43e326315e54b6a58ce74950f4f758d6cba0c64a
MaceWindu <MaceWindu@users.noreply.github.com>
2020-09-25 10:49:26 +0300

    [Linux / NETCOREAPP2.1 / SQL Server 2017 (System.Data.SqlClient)] baselines from https://github.com/linq2db/linq2db/pull/2455 (#355)
    Co-authored-by: Azure Pipelines Bot <azp@linq2db.com>

---
+++ core21/Northwind/Tests/Linq/FullTextTests/Tests.Linq.FullTextTests.ContainsTableByColumnLanguageName(Northwind).sql
@@ -0,0 +1,18 @@
+﻿BeforeExecute
+-- Northwind SqlServer.2017
+DECLARE @p1 NVarChar(4000) -- String
+SET     @p1 = N'meat'
+DECLARE @p2 NVarChar(4000) -- String
+SET     @p2 = N'Turkish'
+
+SELECT
+	[c_1].[CategoryID],
+	[c_1].[CategoryName],
+	[c_1].[Description],
+	[c_1].[Picture]
+FROM
+	[Categories] [c_1]
+		INNER JOIN CONTAINSTABLE([Categories], ([CategoryName]), @p1, LANGUAGE @p2) [t] ON [c_1].[CategoryID] = [t].[KEY]
+ORDER BY
+	[t].[RANK] DESC
+
---
+++ core21/Northwind/Tests/Linq/FullTextTests/Tests.Linq.FullTextTests.FreeTextTableByAllAsExpressionMethod(Northwind).sql
@@ -0,0 +1,16 @@
+﻿BeforeExecute
+-- Northwind SqlServer.2017
+
+SELECT
+	[c_1].[CategoryID],
+	[c_1].[CategoryName],
+	[c_1].[Description],
+	[c_1].[Picture]
+FROM
+	[Categories] [c_1]
+		CROSS APPLY FREETEXTTABLE([Categories], *, N'seafood bread') [t]
+WHERE
+	[c_1].[CategoryID] = [t].[KEY]
+ORDER BY
+	[t].[RANK] DESC
+
