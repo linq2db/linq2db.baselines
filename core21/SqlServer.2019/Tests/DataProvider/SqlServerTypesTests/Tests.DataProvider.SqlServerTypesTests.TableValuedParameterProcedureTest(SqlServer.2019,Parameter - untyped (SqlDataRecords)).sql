d3c7845f557232e988073eae3925285b46b6dbc8
MaceWindu <MaceWindu@users.noreply.github.com>
2020-09-25 13:47:30 +0300

    [Windows / NETCOREAPP2.1 / SQL Server 2019 (System.Data.SqlClient)] baselines from https://github.com/linq2db/linq2db/pull/2455 (#408)
    Co-authored-by: Azure Pipelines Bot <azp@linq2db.com>

---
+++ core21/SqlServer.2019/Tests/DataProvider/SqlServerTypesTests/Tests.DataProvider.SqlServerTypesTests.TableValuedParameterProcedureTest(SqlServer.2019,Parameter - untyped (SqlDataRecords)).sql
@@ -0,0 +1,7 @@
+﻿BeforeExecute
+-- SqlServer.2019 SqlServer.2017
+DECLARE @table Structured -- Object
+SET     @table = Tests.DataProvider.SqlServerTypesTests+<GetSqlDataRecords>d__22
+
+TableTypeTestProc
+
