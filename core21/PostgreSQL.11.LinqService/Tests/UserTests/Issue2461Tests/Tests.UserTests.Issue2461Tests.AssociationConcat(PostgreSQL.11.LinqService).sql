1ada0ee329df1fba4d2cb3ae0461ca40e0607e65
MaceWindu <MaceWindu@users.noreply.github.com>
2020-09-25 10:44:40 +0300

    [Linux / NETCOREAPP2.1 / PostgreSQL 11] baselines from https://github.com/linq2db/linq2db/pull/2455 (#367)
    Co-authored-by: Azure Pipelines Bot <azp@linq2db.com>

---
+++ core21/PostgreSQL.11.LinqService/Tests/UserTests/Issue2461Tests/Tests.UserTests.Issue2461Tests.AssociationConcat(PostgreSQL.11.LinqService).sql
@@ -0,0 +1,28 @@
+﻿BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL
+
+CREATE TABLE "MRECEIPT"
+(
+	"RECEIPT_NO" text     NULL,
+	"CUSTKEY"    text     NULL
+)
+
+BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL
+
+CREATE TABLE "CUST_DTL"
+(
+	"CUSTKEY"   text     NULL,
+	"BILLGROUP" text     NULL
+)
+
+BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL
+
+DROP TABLE "CUST_DTL"
+
+BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL
+
+DROP TABLE "MRECEIPT"
+
---
+++ core21/PostgreSQL.11/Tests/xUpdate/CreateTempTableTests/Tests.xUpdate.CreateTempTableTests.CreateTableAsyncCanceled2(PostgreSQL.11).sql
@@ -0,0 +1,35 @@
+﻿BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL
+
+DROP TABLE IF EXISTS "TempTable"
+
+BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL (asynchronously)
+
+CREATE TABLE "TempTable"
+(
+	"ID" Int NOT NULL
+)
+
+BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL (asynchronously)
+
+INSERT INTO "TempTable"
+(
+	"ID"
+)
+SELECT
+	p."ParentID"
+FROM
+	"Parent" p
+
+BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL (asynchronously)
+
+DROP TABLE "TempTable"
+
+BeforeExecute
+-- PostgreSQL.11 PostgreSQL.9.5 PostgreSQL
+
+DROP TABLE "TempTable"
+
