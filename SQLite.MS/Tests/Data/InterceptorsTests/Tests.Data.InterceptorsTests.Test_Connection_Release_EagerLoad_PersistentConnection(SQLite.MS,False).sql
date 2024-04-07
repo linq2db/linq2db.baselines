b749da50d05cf59b3946c31c0798567f6fb7b403
MaceWindu <MaceWindu@users.noreply.github.com>
2024-04-07 19:38:13 +0200

    Baselines v5.4.1

---
+++ SQLite.MS/Tests/Data/InterceptorsTests/Tests.Data.InterceptorsTests.Test_Connection_Release_EagerLoad_PersistentConnection(SQLite.MS,False).sql
@@ -0,0 +1,204 @@
+﻿BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[t1].[FirstName],
+	[t1].[PersonID],
+	[t1].[LastName],
+	[t1].[MiddleName],
+	[t1].[Gender],
+	[a_Patient].[PersonID],
+	[a_Patient].[Diagnosis]
+FROM
+	[Person] [t1]
+		LEFT JOIN [Patient] [a_Patient] ON [t1].[PersonID] = [a_Patient].[PersonID]
+
+BeforeExecute
+BeginTransaction(Serializable)
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[lw_Parent].[ParentID],
+	[detail].[ParentID],
+	[detail].[ChildID]
+FROM
+	(
+		SELECT DISTINCT
+			[t1].[ParentID]
+		FROM
+			[Parent] [t1]
+	) [lw_Parent]
+		INNER JOIN [Child] [detail] ON [lw_Parent].[ParentID] = [detail].[ParentID]
+
+BeforeExecute
+DisposeTransaction
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[t1].[ParentID],
+	[t1].[Value1]
+FROM
+	[Parent] [t1]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[t1].[FirstName],
+	[t1].[PersonID],
+	[t1].[LastName],
+	[t1].[MiddleName],
+	[t1].[Gender],
+	[a_Patient].[PersonID],
+	[a_Patient].[Diagnosis]
+FROM
+	[Person] [t1]
+		LEFT JOIN [Patient] [a_Patient] ON [t1].[PersonID] = [a_Patient].[PersonID]
+
+BeforeExecute
+BeginTransaction(Serializable)
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[lw_Parent].[ParentID],
+	[detail].[ParentID],
+	[detail].[ChildID]
+FROM
+	(
+		SELECT DISTINCT
+			[t1].[ParentID]
+		FROM
+			[Parent] [t1]
+	) [lw_Parent]
+		INNER JOIN [Child] [detail] ON [lw_Parent].[ParentID] = [detail].[ParentID]
+
+BeforeExecute
+DisposeTransaction
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[t1].[ParentID],
+	[t1].[Value1]
+FROM
+	[Parent] [t1]
+
+BeforeExecute
+-- SQLite.MS SQLite
+DECLARE @take  -- Int32
+SET     @take = 1
+
+SELECT
+	[t1].[FirstName],
+	[t1].[PersonID],
+	[t1].[LastName],
+	[t1].[MiddleName],
+	[t1].[Gender],
+	[a_Patient].[PersonID],
+	[a_Patient].[Diagnosis]
+FROM
+	[Person] [t1]
+		LEFT JOIN [Patient] [a_Patient] ON [t1].[PersonID] = [a_Patient].[PersonID]
+LIMIT @take
+
+BeforeExecute
+BeginTransaction(Serializable)
+BeforeExecute
+-- SQLite.MS SQLite
+DECLARE @take  -- Int32
+SET     @take = 1
+
+SELECT
+	[lw_Parent].[ParentID],
+	[detail].[ParentID],
+	[detail].[ChildID]
+FROM
+	(
+		SELECT DISTINCT
+			[t2].[ParentID]
+		FROM
+			(
+				SELECT
+					[t1].[ParentID]
+				FROM
+					[Parent] [t1]
+				LIMIT @take
+			) [t2]
+	) [lw_Parent]
+		INNER JOIN [Child] [detail] ON [lw_Parent].[ParentID] = [detail].[ParentID]
+
+BeforeExecute
+-- SQLite.MS SQLite
+DECLARE @take  -- Int32
+SET     @take = 1
+
+SELECT
+	[t1].[ParentID],
+	[t1].[Value1]
+FROM
+	[Parent] [t1]
+LIMIT @take
+
+BeforeExecute
+DisposeTransaction
+BeforeExecute
+-- SQLite.MS SQLite (asynchronously)
+DECLARE @take  -- Int32
+SET     @take = 1
+
+SELECT
+	[t1].[FirstName],
+	[t1].[PersonID],
+	[t1].[LastName],
+	[t1].[MiddleName],
+	[t1].[Gender],
+	[a_Patient].[PersonID],
+	[a_Patient].[Diagnosis]
+FROM
+	[Person] [t1]
+		LEFT JOIN [Patient] [a_Patient] ON [t1].[PersonID] = [a_Patient].[PersonID]
+LIMIT @take
+
+BeforeExecute
+BeginTransactionAsync(Serializable)
+BeforeExecute
+-- SQLite.MS SQLite (asynchronously)
+DECLARE @take  -- Int32
+SET     @take = 1
+
+SELECT
+	[lw_Parent].[ParentID],
+	[detail].[ParentID],
+	[detail].[ChildID]
+FROM
+	(
+		SELECT DISTINCT
+			[t2].[ParentID]
+		FROM
+			(
+				SELECT
+					[t1].[ParentID]
+				FROM
+					[Parent] [t1]
+				LIMIT @take
+			) [t2]
+	) [lw_Parent]
+		INNER JOIN [Child] [detail] ON [lw_Parent].[ParentID] = [detail].[ParentID]
+
+BeforeExecute
+-- SQLite.MS SQLite (asynchronously)
+DECLARE @take  -- Int32
+SET     @take = 1
+
+SELECT
+	[t1].[ParentID],
+	[t1].[Value1]
+FROM
+	[Parent] [t1]
+LIMIT @take
+
+BeforeExecute
+DisposeTransactionAsync
