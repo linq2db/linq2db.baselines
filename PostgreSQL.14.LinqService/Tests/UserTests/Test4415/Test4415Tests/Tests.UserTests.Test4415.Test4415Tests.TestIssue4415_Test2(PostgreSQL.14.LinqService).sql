c52f71bc72573da377c492de1c738b766fcfb27c
MaceWindu <MaceWindu@users.noreply.github.com>
2024-05-03 10:38:24 +0200

    duplicates

--- PostgreSQL.14.LinqService/Tests/UserTests/Test4415/Test4415Tests/Tests.UserTests.Test4415.Test4415Tests.TestIssue4415_Test2(PostgreSQL.14.LinqService).sql
+++ PostgreSQL.14.LinqService/Tests/UserTests/Test4415/Test4415Tests/Tests.UserTests.Test4415.Test4415Tests.TestIssue4415_Test2(PostgreSQL.14.LinqService).sql
@@ -41,16 +41,11 @@ FROM
 WHERE
 	x."LanguageID" IN (
 		SELECT
-			t1."MAX_1"
+			MAX(x_1."LanguageID")
 		FROM
-			(
-				SELECT
-					MAX(x_1."LanguageID") as "MAX_1"
-				FROM
-					"Common_Language" x_1
-				GROUP BY
-					x_1."Name"
-			) t1
+			"Common_Language" x_1
+		GROUP BY
+			x_1."Name"
 	)
 
 BeforeExecute
