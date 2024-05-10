77a54a7fb7fc54604580c76482ecf47c211c3d0e
MaceWindu <MaceWindu@users.noreply.github.com>
2024-05-10 12:22:22 +0200

    add v5 baselines for new tests

---
+++ SQLite.MS.LinqService/Tests/Linq/EagerLoadingTests/Tests.Linq.EagerLoadingTests.TestReadOnlyAssociationSingleNoData(SQLite.MS.LinqService,3).sql
@@ -0,0 +1,116 @@
+﻿BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityA]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityA]
+(
+	[Id] INTEGER NOT NULL,
+	[FK] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityA] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityB]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityB]
+(
+	[Id] INTEGER NOT NULL,
+	[FK] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityB] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityC]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityC]
+(
+	[Id] INTEGER NOT NULL,
+	[FK] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityC] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityD]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityD]
+(
+	[Id] INTEGER NOT NULL,
+	[FK] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityD] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[lw_EntityB].[Id],
+	[detail].[Id],
+	[detail].[FK]
+FROM
+	(
+		SELECT DISTINCT
+			[a_ObjectB].[Id]
+		FROM
+			[EntityA] [t1]
+				INNER JOIN [EntityB] [a_ObjectB] ON [t1].[FK] = [a_ObjectB].[Id]
+	) [lw_EntityB]
+		INNER JOIN [EntityD] [detail] ON [lw_EntityB].[Id] = [detail].[FK]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[t1].[Id],
+	[t1].[FK],
+	[a_ObjectB].[Id],
+	[a_ObjectB].[FK],
+	[a_ObjectC].[Id],
+	[a_ObjectC].[FK]
+FROM
+	[EntityA] [t1]
+		INNER JOIN [EntityB] [a_ObjectB] ON [t1].[FK] = [a_ObjectB].[Id]
+		LEFT JOIN [EntityC] [a_ObjectC] ON [a_ObjectB].[FK] = [a_ObjectC].[Id]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityD]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityC]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityB]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityA]
+
---
+++ SQLite.MS/Tests/Linq/EagerLoadingTests/Tests.Linq.EagerLoadingTests.TestReadOnlyAssociationManyNoData(SQLite.MS,2).sql
@@ -0,0 +1,132 @@
+﻿BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMA]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityMA]
+(
+	[Id] INTEGER NOT NULL,
+	[FK] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityMA] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMB]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityMB]
+(
+	[Id]  INTEGER NOT NULL,
+	[FK]  INTEGER     NULL,
+	[FKD] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityMB] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMC]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityMC]
+(
+	[Id] INTEGER NOT NULL,
+	[FK] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityMC] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMD]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+CREATE TABLE IF NOT EXISTS [EntityMD]
+(
+	[Id] INTEGER NOT NULL,
+	[FK] INTEGER     NULL,
+
+	CONSTRAINT [PK_EntityMD] PRIMARY KEY ([Id])
+)
+
+BeforeExecute
+BeginTransaction(Serializable)
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[lw_EntityMB].[Id_1],
+	[lw_EntityMB].[Id],
+	[detail_1].[Id],
+	[detail_1].[FK]
+FROM
+	(
+		SELECT DISTINCT
+			[detail].[Id],
+			[lw_EntityMA].[Id] as [Id_1]
+		FROM
+			[EntityMA] [lw_EntityMA]
+				INNER JOIN [EntityMB] [detail] ON [lw_EntityMA].[Id] = [detail].[FK]
+	) [lw_EntityMB]
+		INNER JOIN [EntityMC] [detail_1] ON [lw_EntityMB].[Id] = [detail_1].[FK]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[lw_EntityMA].[Id],
+	[detail].[Id],
+	[detail].[FK],
+	[detail].[FKD],
+	[a_ObjectD].[Id],
+	[a_ObjectD].[FK]
+FROM
+	[EntityMA] [lw_EntityMA]
+		INNER JOIN [EntityMB] [detail] ON [lw_EntityMA].[Id] = [detail].[FK]
+		LEFT JOIN [EntityMD] [a_ObjectD] ON [detail].[FKD] = [a_ObjectD].[Id]
+
+BeforeExecute
+DisposeTransaction
+BeforeExecute
+-- SQLite.MS SQLite
+
+SELECT
+	[t1].[Id],
+	[t1].[FK]
+FROM
+	[EntityMA] [t1]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMD]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMC]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMB]
+
+BeforeExecute
+-- SQLite.MS SQLite
+
+DROP TABLE IF EXISTS [EntityMA]
+
