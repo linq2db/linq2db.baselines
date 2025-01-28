0a840ac6a4e37024ca0c3c7034ec4833342c2e30
MaceWindu <MaceWindu@users.noreply.github.com>
2025-01-28 10:17:42 +0100

    Baselines v6.0.0-preview.3

---
+++ DB2/Tests/Linq/InterfaceTests/Tests.Linq.InterfaceTests.ExtensionRegression(DB2).sql
@@ -0,0 +1,11 @@
+﻿BeforeExecute
+-- DB2 DB2.LUW DB2LUW
+
+SELECT DISTINCT
+	"a_Child".ID
+FROM
+	"ExtensionTable1" "r"
+		LEFT JOIN "ExtensionTable2" "a_Child" ON "r".FK = "a_Child".ID
+WHERE
+	"a_Child".ID IS NOT NULL
+
---
+++ MySql.5.7/Tests/Linq/InterfaceTests/Tests.Linq.InterfaceTests.ExtensionRegression(MySql.5.7).sql
@@ -0,0 +1,11 @@
+﻿BeforeExecute
+-- MySql.5.7 MySql.5.7.MySql.Data MySql57
+
+SELECT DISTINCT
+	`a_Child`.`ID`
+FROM
+	`ExtensionTable1` `r`
+		LEFT JOIN `ExtensionTable2` `a_Child` ON `r`.`FK` = `a_Child`.`ID`
+WHERE
+	`a_Child`.`ID` IS NOT NULL
+
