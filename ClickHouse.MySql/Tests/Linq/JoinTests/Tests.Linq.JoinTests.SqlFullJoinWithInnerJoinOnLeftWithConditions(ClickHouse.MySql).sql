3404be709368178e78e3eb27788dd05124f25b53
MaceWindu <MaceWindu@users.noreply.github.com>
2025-06-28 17:07:45 +0200

    reviewed: aliases

--- ClickHouse.MySql/Tests/Linq/JoinTests/Tests.Linq.JoinTests.SqlFullJoinWithInnerJoinOnLeftWithConditions(ClickHouse.MySql).sql
+++ ClickHouse.MySql/Tests/Linq/JoinTests/Tests.Linq.JoinTests.SqlFullJoinWithInnerJoinOnLeftWithConditions(ClickHouse.MySql).sql
@@ -2,13 +2,13 @@
 -- ClickHouse.MySql ClickHouse
 
 SELECT
-	t1.ParentID,
-	right_2.cond
+	t1.Left_1,
+	right_2.Right_1
 FROM
 	(
 		SELECT
 			p.Value1 as Value1,
-			p.ParentID as ParentID
+			p.ParentID as Left_1
 		FROM
 			Parent p
 		WHERE
@@ -16,7 +16,7 @@ FROM
 	) t1
 		FULL JOIN (
 			SELECT
-				right_1.ParentID as cond,
+				right_1.ParentID as Right_1,
 				right_1.Value1 + 2 as c1
 			FROM
 				Parent right_1
@@ -25,5 +25,5 @@ FROM
 				right_1.ParentID <> 2 AND right2.ParentID <> 1
 		) right_2 ON right_2.c1 = t1.Value1 OR right_2.c1 IS NULL AND t1.Value1 IS NULL
 ORDER BY
-	t1.ParentID
+	t1.Left_1
 
