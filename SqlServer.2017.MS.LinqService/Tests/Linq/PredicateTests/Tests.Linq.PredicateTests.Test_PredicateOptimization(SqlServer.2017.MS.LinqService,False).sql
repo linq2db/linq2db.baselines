4030c3edb6182f015be07a1dc599bd00d98acb41
MaceWindu <MaceWindu@users.noreply.github.com>
2025-07-06 13:39:07 +0200

    reviewed

--- SqlServer.2017.MS.LinqService/Tests/Linq/PredicateTests/Tests.Linq.PredicateTests.Test_PredicateOptimization(SqlServer.2017.MS.LinqService,False).sql
+++ SqlServer.2017.MS.LinqService/Tests/Linq/PredicateTests/Tests.Linq.PredicateTests.Test_PredicateOptimization(SqlServer.2017.MS.LinqService,False).sql
@@ -778,3 +778,465 @@ SELECT
 FROM
 	[BooleanTable] [t1]
 
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] > [r].[Value2] OR [r].[Value1] <= [r].[Value2]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] >= [r].[Value2] OR [r].[Value1] < [r].[Value2]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] < [r].[Value4] OR [r].[Value1] >= [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] > [r].[Value4] OR [r].[Value1] <= [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value5] > [r].[Value4] OR [r].[Value5] <= [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value5] >= [r].[Value4] OR [r].[Value5] < [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	1 = 0
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] > [r].[Value2] AND [r].[Value1] <= [r].[Value2]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] >= [r].[Value2] AND [r].[Value1] < [r].[Value2]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	1 = 0
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] < [r].[Value4] AND [r].[Value1] >= [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value1] > [r].[Value4] AND [r].[Value1] <= [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	1 = 0
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value5] > [r].[Value4] AND [r].[Value5] <= [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[r].[Id],
+	[r].[Value1],
+	[r].[Value2],
+	[r].[Value4],
+	[r].[Value5]
+FROM
+	[BooleanTable] [r]
+WHERE
+	[r].[Value5] >= [r].[Value4] AND [r].[Value5] < [r].[Value4]
+
+BeforeExecute
+-- SqlServer.2017.MS SqlServer.2017
+
+SELECT
+	[t1].[Id],
+	[t1].[Value1],
+	[t1].[Value2],
+	[t1].[Value4],
+	[t1].[Value5]
+FROM
+	[BooleanTable] [t1]
+
